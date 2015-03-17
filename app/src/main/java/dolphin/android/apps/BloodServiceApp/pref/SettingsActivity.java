90f6b57c76c49ebb09113c7ed25e01d976e4d73e
DolphinWing <dolphinwing74+github@gmail.com>
2015-03-18 00:55:22 +0800

    r36
    	modified:   app/build.gradle
    	modified:   app/src/main/java/dolphin/android/apps/BloodServiceApp/pref/SettingsActivity.java
    	new file:   app/src/main/res/layout/settings_toolbar.xml

--- app/src/main/java/dolphin/android/apps/BloodServiceApp/pref/SettingsActivity.java
+++ app/src/main/java/dolphin/android/apps/BloodServiceApp/pref/SettingsActivity.java
@@ -17,7 +17,11 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
@@ -70,6 +74,16 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         //setupSimplePreferencesScreen();
         new Handler().post(new Runnable() {
