9b3757b1ef424303fa7a7786734ffa1fbb101d14
DolphinWing <dolphinwing74+github@gmail.com>
2018-06-23 11:49:45 +0800

    remove some files to track

--- app/src/main/java/dolphin/android/apps/BloodServiceApp/provider/LocaleUtil.kt
+++ app/src/main/java/dolphin/android/apps/BloodServiceApp/provider/LocaleUtil.kt
@@ -16,20 +16,21 @@ class LocaleUtil {
             val locale = Locale("zh", "TW")
             Locale.setDefault(locale)
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                return updateResources(context, locale)
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                updateResources(context, locale)
             else
-                return updateResourcesLegacy(context, locale)
+                updateResourcesLegacy(context, locale)
         }
 
         @TargetApi(Build.VERSION_CODES.N)
         private fun updateResources(context: Context, locale: Locale): Context {
             val configuration = context.resources.configuration
-            configuration.locale = locale
+            configuration.setLocale(locale)
             configuration.setLayoutDirection(locale)
             return context.createConfigurationContext(configuration)
         }
 
+        @Suppress("DEPRECATION")
         private fun updateResourcesLegacy(context: Context, locale: Locale): Context {
             val resources = context.resources
             val configuration = resources.configuration
